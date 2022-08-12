a08fd4ae22e20226501db682ee62075c5b88603d
Jownsu <jhonesdigno666@gmail.com>
2022-08-13 01:48:43 +0800

    redesigned the clickable prototype and added sql dummy data for products

---
+++ assets/category.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (name, created_at, updated_at) VALUES 
+('keyboard', NOW(), NOW()),
+('monitor', NOW(), NOW()),
+('motherboard', NOW(), NOW()),
+('mouse', NOW(), NOW()),
+('pc case', NOW(), NOW()),
+('powerbank', NOW(), NOW())
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-#Village 88 - Capstone E-commerce
+# Village 88 - Capstone E-commerce
 
 ## ERD
 ![This is the ERD of the e-commerce project](/assets/erd.png "E-commerce ERD")
